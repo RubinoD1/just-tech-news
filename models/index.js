ae67757b8caaa8ee4ce08c85bfa375a2787a5b5e
RubinoD1 <Rubinod1@gmail.com>
2022-07-29 00:13:14 -0400

    refactored code and added vote routes

--- models/index.js
+++ models/index.js
@@ -1,13 +1,22 @@
 const User = require("./User");
 const Post = require("./Post");
+const Vote = require('./Vote');
 
-// create associations
-User.hasMany(Post, {
-    foreignKey: 'user_id'
-  });
 
-Post.belongsTo(User, {
-   foreignKey: 'user_id',
- });
+Vote.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
-module.exports = { User, Post };
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
