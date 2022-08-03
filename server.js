25219e860374b7c7f21f1d64f49a53f4a50fa2c7
RubinoD1 <Rubinod1@gmail.com>
2022-08-03 14:45:31 -0400

    added login authentication

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const routes = require('./controllers');
 const path = require('path');
 const sequelize = require('./config/connection');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
@@ -9,9 +12,20 @@ const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
